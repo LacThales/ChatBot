764abbf5e1b2ce9c4b70d384ad85c1faff40404b
Thales de Oliveira Lacerda <63318165+LacThales@users.noreply.github.com>
2023-02-27 23:41:04 -0300

    Add files via upload
    apenas rodar o main e funcionará as interações

---
+++ main.py
@@ -0,0 +1,32 @@
+from chatbot import ChatBot
+myChatBot = ChatBot()
+#apenas carregar um modelo pronto
+myChatBot.loadModel()
+
+#criar o modelo e treinar
+myChatBot.createModel()
+
+print("Bem vindo ao Chatbot\n\n")      
+sinonimos_sim = ["sim", "claro", "óbvio", "yes"]
+
+pergunta = input("como posso te ajudar?\n\n")
+resposta, intencao = myChatBot.chatbot_response(pergunta)
+print(resposta + "   ["+intencao[0]['intent']+"]")
+
+while (intencao[0]['intent']!="despedida"):
+    if (resposta.__contains__("Iae, bem sim e vc?")):
+        input("")
+    if (resposta.__contains__("TCC?")):
+        pergunta = input("")
+        resposta, intencao = myChatBot.chatbot_response(pergunta)
+        print(resposta + "   [" + intencao[0]['intent'] + "]")
+    if(resposta.__contains__("TCC") and intencao[0]['intent'] == "questionamento_entregas"):
+        resposta = input("")
+        if resposta in sinonimos_sim:
+            resposta, intencao = myChatBot.chatbot_response(resposta, 1)
+            print(resposta + "   [" + intencao[0]['intent'] + "]")
+    pergunta = input("Posso lhe ajudar com algo a mais?\n")
+    resposta, intencao = myChatBot.chatbot_response(pergunta)
+    print(resposta + "   [" + intencao[0]['intent'] + "]")
+
+print("Foi um prazer atendê-lo")
